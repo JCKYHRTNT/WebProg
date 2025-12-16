c8ecd68e9f2ba4f8acf4976af0110ea66e18cfef
jackyhertanto <hertantojacky2@gmail.com>
2025-12-16 22:08:20 +0700

    Delete local backup (accident)

--- testproject/storage/framework/views/93ada80c891796579482e2f993d9c58b.php
+++
@@ -1,5 +0,0 @@
-<footer class="tb-footer">
-    <div class="tb-container text-center">
-        &copy; 2025 The Boys
-    </div>
-</footer><?php /**PATH D:\Abraham Folder\WebProg\testproject\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
